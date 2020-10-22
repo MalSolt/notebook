77ef76239ab3d0b5176226951a09b6e10db5509d
Malik Soltukiev <sma_fcb@xxx.yyy>
2020-10-22 11:30:47 +0300

    Updates

---
+++ precache-manifest.0e4190179087f19b4970f55e26de3fb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621a0ade87a8e360f37499a025fb82ec",
+    "url": "/notebook/index.html"
+  },
+  {
+    "revision": "94484328bcdeaaa01d15",
+    "url": "/notebook/static/css/main.09c7fc8d.chunk.css"
+  },
+  {
+    "revision": "b349f62658bbe04b52a9",
+    "url": "/notebook/static/js/2.ca6d83fb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/notebook/static/js/2.ca6d83fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94484328bcdeaaa01d15",
+    "url": "/notebook/static/js/main.f8c9157f.chunk.js"
+  },
+  {
+    "revision": "344e39447fdae9e23630",
+    "url": "/notebook/static/js/runtime-main.fdcc326c.js"
+  },
+  {
+    "revision": "7761680aedcd2424aebaa4579f3a6d55",
+    "url": "/notebook/static/media/arrow.7761680a.svg"
+  }
+]);
